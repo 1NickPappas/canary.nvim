7ccc81080bc0d0a1cb16c8a4b5ac452d958f2ca4
Nick Pappas <nick.pappas.ee@gmail.com>
2025-12-30 21:05:18 +0200

    feat: add plugin commands
    - :CanaryCheck - Check dependency versions
    - :CanaryShow - Show version hints
    - :CanaryHide - Hide version hints
    - :CanaryToggle - Toggle version hints
    - :CanaryRefresh - Force refresh (bypass cache)

--- plugin/canary.lua
+++ plugin/canary.lua
@@ -2,3 +2,23 @@ if vim.g.loaded_canary then
   return
 end
 vim.g.loaded_canary = true
+
+vim.api.nvim_create_user_command("CanaryCheck", function()
+  require("canary").check()
+end, { desc = "Check dependency versions" })
+
+vim.api.nvim_create_user_command("CanaryShow", function()
+  require("canary").show()
+end, { desc = "Show version hints" })
+
+vim.api.nvim_create_user_command("CanaryHide", function()
+  require("canary").hide()
+end, { desc = "Hide version hints" })
+
+vim.api.nvim_create_user_command("CanaryToggle", function()
+  require("canary").toggle()
+end, { desc = "Toggle version hints" })
+
+vim.api.nvim_create_user_command("CanaryRefresh", function()
+  require("canary").refresh()
+end, { desc = "Force refresh (bypass cache)" })
