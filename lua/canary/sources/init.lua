832fca6635af9a105339c266d6dde0af1382a35d
Nick Pappas <nick.pappas.ee@gmail.com>
2025-12-30 21:01:29 +0200

    feat(source): add source registry
    - Register npm and cargo sources
    - get(): Retrieve source by filetype

--- lua/canary/sources/init.lua
+++ lua/canary/sources/init.lua
@@ -1,3 +1,12 @@
 local M = {}
 
+local sources = {
+  npm = require("canary.sources.npm"),
+  cargo = require("canary.sources.cargo"),
+}
+
+function M.get(filetype)
+  return sources[filetype]
+end
+
 return M
